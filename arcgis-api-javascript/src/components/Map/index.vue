9d96e9cb910d33d65b060691a9ac17cac25f4107
WayneWu <wayne-wu@outlook.com>
2022-06-11 22:40:39 +0800

    map

--- arcgis-api-javascript/src/components/Map/index.vue
+++ arcgis-api-javascript/src/components/Map/index.vue
@@ -1,16 +1,50 @@
 <template>
   <div id="viewDiv"></div>
+  <div id="cityList">
+    <ol>
+      <li v-for="city in cities" :key="city" v-on:click="onclick(city)">
+        {{ city }}
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
-import { init } from "./utils";
+/* eslint-disable */
+import { init, createGraphic } from "./utils";
+import cityList from "@/config/cities.json";
+import { getCityJson } from "@/api/index.js";
+
+let map = null;
+let view = null;
+let layer = null;
+let collection = null;
 
 export default {
   name: "Json_map",
-  mounted() {
-    init();
+  data() {
+    return {
+      cities: cityList,
+    };
   },
-  methods: {},
+  mounted() {
+    ({ map, view, layer, collection } = init());
+  },
+  methods: {
+    onclick(city) {
+      getCityJson(city).then((data) => {
+        for (const feat of data.features) {
+          const graphics = feat.geometry.coordinates.map((coords) =>
+            createGraphic(coords)
+          );
+
+          graphics.forEach((graphic) => {
+            collection.add(graphic);
+          });
+        }
+      });
+    },
+  },
 };
 </script>
 
@@ -21,5 +55,26 @@ body,
   padding: 0;
   margin: 0;
   height: 100vh;
+}
+
+#cityList {
+  position: absolute;
+  bottom: 100px;
+  right: 0;
+  padding: 0 0 0 0;
+  width: 150px;
+  height: 245px;
+  background-color: rgba(0, 89, 179, 0.3);
+  text-align: center;
+}
+
+#cityList ol {
+  display: block;
+  position: absolute;
+  right: 0;
+  width: 150px;
+  height: 200px;
+  list-style: none;
+  overflow-y: scroll;
 }
 </style>
--- arcgis-api-javascript/src/components/Map/utils.js
+++ arcgis-api-javascript/src/components/Map/utils.js
@@ -1,25 +1,40 @@
 /* eslint-disable */
-import citiesArray from "@/config/cities.json";
-import Map from "@arcgis/core/Map";
-import MapView from "@arcgis/core/views/MapView";
-import { getCityJson } from "@/api/index.js"
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+import Collection from '@arcgis/core/core/Collection';
+import Graphic from '@arcgis/core/Graphic';
 
 export function init() {
-    const map = new Map({
-        basemap: "topo-vector"
-    });
+  const map = new Map({
+    basemap: 'topo-vector',
+  });
 
-    const view = new MapView({
-        container: "viewDiv",
-        center: [95, 32],
-        zoom: 4,
-        map: map
-    });
+  const view = new MapView({
+    container: 'viewDiv',
+    center: [95, 32],
+    zoom: 4,
+    map: map,
+  });
 
-    // 导入Json数据
-    citiesArray.forEach(city => {
-        getCityJson(city).then(data => {
-            console.log(data);
-        })
-    });
+  const collection = new Collection();
+
+  const layer = new FeatureLayer({
+    title: 'map layer',
+    geometryType: 'polygon',
+    source: collection,
+  });
+
+  map.add(layer);
+
+  return { map, layer, view, collection };
+}
+
+export function createGraphic(coords) {
+  return new Graphic({
+    geometry: {
+      type: 'polygon',
+      rings: coords,
+    },
+  });
 }
